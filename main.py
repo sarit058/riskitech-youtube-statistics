5cbc5485bad73cfa0e60d5a201eaee4cbf265d53
Sarit Soloduchin <sarit@some-email>
2020-05-02 23:34:59 +0300

    Iserting data to the table from the csv file

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class mydatabase:
         self.file_name = file_name[0:8]
         ## READING A CSV FILE
         country_videos = pd.read_csv(file_name)
-
+        
         ## COLUMNS NAME AND TYPE
         column_name = country_videos.columns
         column_type =country_videos.dtypes.iloc
@@ -48,17 +48,43 @@ class mydatabase:
                 column_type[i]='float'                      
             if i == 0:
                 text = column_name[i] + ' ' + column_type[i]
+                column_text = column_name[i]
             else:
                 text = text + ', ' + column_name[i] + ' ' + column_type[i]
+                column_text = column_text + ', ' + column_name[i]
+            
         ## CREATING THE TABLE
         cur.execute("CREATE TABLE {0!s} ({1!s});".format(self.file_name, text))
-        # cur.execute("DROP TABLE {0!s}".format(self.file_name)) 
+        
+        
+        ## INSERTING DATA INTO THE TABLE
+        for i in range(len(country_videos.index)):
+            a = country_videos.values[i].tolist()
+            
+            # CHECK FOR BAD CHARACTER: '
+            for j in range(len(column_name)):
+                if type(a[j]) == str:
+                    a[j] = a[j].replace("'", '')
+        
+            seperator = "', '"
+            astr ="'" + seperator.join(str(x) for x in a) + "'"
+      
+        
+            cur.execute("INSERT INTO {0!s} VALUES ({2!s});".format(self.file_name,  column_text, astr))
+
+
+    def del_data(self, file_name):
+        self.file_name = file_name[0:8]
+
+        ## DELETING THE TABLE
+        cur.execute("DROP TABLE {0!s}".format(self.file_name))
 
 
 mdb = mydatabase('youtube')
 # mdb.init_database()
 
 mdb.init_data('USvideos.csv')
+# mdb.del_data('USvideos.csv')
 
 con.close()
 
